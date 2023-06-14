d26d93dae00cf3729151be0477489432610715b5
t-kuzuma <t-kuzuma@ruby-dev.jp>
2023-06-14 15:03:46 +0900

    rubocop erblint 修正

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,16 +3,10 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
-  has_many :mentioned_relations, class_name: 'Mention',
-                                  foreign_key: 'mentioned_report_id',
-                                  dependent: :destroy
-  has_many :mentioned_reports, through: :mentioned_relations,
-                                source: :mentioning_report
-  has_many :mentioning_relations, class_name: 'Mention',
-                                  foreign_key: 'mentioning_report_id',
-                                  dependent: :destroy
-  has_many :mentioning_reports, through: :mentioning_relations,
-                                source: :mentioned_report
+  has_many :mentioned_relations, class_name: 'Mention', foreign_key: 'mentioned_report_id', dependent: :destroy, inverse_of: :mentioned_report
+  has_many :mentioned_reports, through: :mentioned_relations, source: :mentioning_report
+  has_many :mentioning_relations, class_name: 'Mention', foreign_key: 'mentioning_report_id', dependent: :destroy, inverse_of: :mentioning_report
+  has_many :mentioning_reports, through: :mentioning_relations, source: :mentioned_report
 
   validates :title, presence: true
   validates :content, presence: true
@@ -26,10 +20,10 @@ class Report < ApplicationRecord
   end
 
   def save_mentions
-    self.mentioning_relations.destroy_all
-    mentioning_ids = self.content.to_s.scan(/http:\/\/localhost:3000\/reports\/(\d+)/).flatten.map(&:to_i).uniq
+    mentioning_relations.destroy_all
+    mentioning_ids = content.to_s.scan(%r{http://localhost:3000/reports/(\d+)}).flatten.map(&:to_i).uniq
     mentioning_ids.each do |mentioning_id|
-      self.mentioning_relations.create!(mentioning_report_id: self.id, mentioned_report_id: mentioning_id)
+      mentioning_relations.create!(mentioning_report_id: id, mentioned_report_id: mentioning_id)
     end
   end
 end
