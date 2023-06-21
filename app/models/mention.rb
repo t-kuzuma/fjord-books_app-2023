27828d5154f5604a3acc69b816c76b43d0d2c84b
t-kuzuma <t-kuzuma@ruby-dev.jp>
2023-06-21 10:44:17 +0900

    モデルレベルでも一意性を保証

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -3,4 +3,6 @@
 class Mention < ApplicationRecord
   belongs_to :mentioned_report, class_name: 'Report'
   belongs_to :mentioning_report, class_name: 'Report'
+
+  validates :mentioned_report_id, uniqueness: { scope: :mentioning_report_id }
 end
